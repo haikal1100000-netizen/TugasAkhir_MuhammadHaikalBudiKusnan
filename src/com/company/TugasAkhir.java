ddd6df7ea78898d6745bbe7c6525537aebc65c3b
haikal1100000-netizen <haikal1100000@gmail.com>
2025-11-24 16:39:29 +0700

    Resolve merge conflict

--- src/com/company/TugasAkhir.java
+++ src/com/company/TugasAkhir.java
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
+
 package com.company;
 
 public class TugasAkhir {
 }
-=======
+
 package com.company;
 
 public class TugasAkhir {
 }
->>>>>>> 8f45bb31e6071c605270b8b7db93c7e26e68d3f0
